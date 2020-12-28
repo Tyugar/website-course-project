7a052f7ba6a8dde9b5f78a3821a87760233d0d7f
Rafal Soczek <rafal032316@gmail.com>
2020-12-28 12:26:34 +0100

    small changes

--- WebProject/src/main/java/project/ProjectApplication.java
+++ WebProject/src/main/java/project/ProjectApplication.java
@@ -2,8 +2,10 @@ package project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, XADataSourceAutoConfiguration.class})
 public class ProjectApplication {
 
 	public static void main(String[] args) {
